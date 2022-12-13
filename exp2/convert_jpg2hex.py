d65670c9d1eb2cfea536bcce5d9d4de93a8e0c49
blackh1 <1594138221@qq.com>
2022-12-13 23:29:32 +0800

    update picture convert

---
+++ exp2/convert_jpg2hex.py
@@ -0,0 +1,5 @@
+from custom_args import ConvertPicture
+
+if __name__ == "__main__":
+    cvtPic=ConvertPicture()
+    cvtPic.pic2hex()
---
+++ exp2/draw.py
@@ -0,0 +1,8 @@
+from custom_args import ConvertPicture
+
+if __name__=="__main__":
+    cvthex=ConvertPicture()
+    cvthex.hex2pic()
+
+
+  
