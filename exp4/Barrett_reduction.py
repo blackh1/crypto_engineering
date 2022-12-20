40bca75f816ff9e97b996b83d94fdd709938f716
Bruce_Lu <luyifu@126.com>
2022-12-21 00:22:40 +0800

    correct the alg.

--- exp4/Barrett_reduction.py
+++ exp4/Barrett_reduction.py
@@ -1,13 +1,13 @@
 # Barrett Reduction
-from math import ceil, log2
+from math import floor
 from random import randint
 
 bit_of_a = 1023
 a = randint(2 ** (bit_of_a - 1) + 1, 2 ** bit_of_a - 1)
 # n is a 1020-bit prime number.
 n = 8862981350823119266272197009064278445415307632595928723347629268121697357234371459742688614077295974018358517166858633371548905750068422858577850776270858439078636697315129123891222696150100229109810079155135885038988834744436834378187222206460578793333037988816236152195003821384440333336888624918459374839
-k = ceil(log2((n-1)*a))  # choice of k is important for correctness of the algorithm
-m = ceil(2**k/n)  # precompute
+k = bit_of_a  # choice of k is important for correctness of the algorithm
+m = floor(2**k/n)  # precompute
 
 def barRedc(a, n):
     q = (a * m) >> k
